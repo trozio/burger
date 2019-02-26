3ad2bc98ab7fc4c26491316c4ac69c77af8a9568
trozio <daniel.l.adelman@gmail.com>
2019-02-26 15:34:53 -0500

    starting logic

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+CREATE TABLE burgers (
+	id INT(10)AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(255),
+	devoured BOOLEAN
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured)
+VALUES(Classic, False);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES(Cheeseburger, False);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES(Bacon Cheeseburger, False);
